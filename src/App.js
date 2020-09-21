8636487cae5af12f75bb681819126cdd5a9aca0d
chakal05 <chakirrasta@gmail.com>
2020-09-21 23:29:30 +0200

    added google map on the landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,199 @@
 import React from 'react';
-import Header from './components/header'
-import './App.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+const MapContainer = () => {
+	const styledMap = [];
 
-function App() {
-  return (
-    <div >
-      <Header/>
-    </div>
-  );
-}
+	const mapStyles = {
+		width: '100%',
+		height: '98vh',
+	
+	};
 
-export default App;
+	const defaultCenter = {
+		lat: 62.4007089,
+		lng: 20.0867185,
+	};
+
+	const key = process.env.REACT_APP_API_KEY;
+
+	return (
+		<LoadScript googleMapsApiKey={key}>
+			<GoogleMap
+				mapContainerStyle={mapStyles}
+				zoom={6}
+				center={defaultCenter}
+			/>
+		</LoadScript>
+	);
+};
+
+export default MapContainer;
+
+// {
+//     featureType: 'all',
+//     elementType: 'geometry.fill',
+//     stylers: [
+//         {
+//             weight: '2.00',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'all',
+//     elementType: 'geometry.stroke',
+//     stylers: [
+//         {
+//             color: '#9c9c9c',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'all',
+//     elementType: 'labels.text',
+//     stylers: [
+//         {
+//             visibility: 'on',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'landscape',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             color: '#f2f2f2',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'landscape',
+//     elementType: 'geometry.fill',
+//     stylers: [
+//         {
+//             color: '#ffffff',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'landscape.man_made',
+//     elementType: 'geometry.fill',
+//     stylers: [
+//         {
+//             color: '#ffffff',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'poi',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             visibility: 'off',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             saturation: -100,
+//         },
+//         {
+//             lightness: 45,
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road',
+//     elementType: 'geometry.fill',
+//     stylers: [
+//         {
+//             color: '#eeeeee',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road',
+//     elementType: 'labels.text.fill',
+//     stylers: [
+//         {
+//             color: '#7b7b7b',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road',
+//     elementType: 'labels.text.stroke',
+//     stylers: [
+//         {
+//             color: '#ffffff',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road.highway',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             visibility: 'simplified',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'road.arterial',
+//     elementType: 'labels.icon',
+//     stylers: [
+//         {
+//             visibility: 'off',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'transit',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             visibility: 'off',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'water',
+//     elementType: 'all',
+//     stylers: [
+//         {
+//             color: '#46bcec',
+//         },
+//         {
+//             visibility: 'on',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'water',
+//     elementType: 'geometry.fill',
+//     stylers: [
+//         {
+//             color: '#c8d7d4',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'water',
+//     elementType: 'labels.text.fill',
+//     stylers: [
+//         {
+//             color: '#070707',
+//         },
+//     ],
+// },
+// {
+//     featureType: 'water',
+//     elementType: 'labels.text.stroke',
+//     stylers: [
+//         {
+//             color: '#ffffff',
+//         },
+//     ],
+// },
